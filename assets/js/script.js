85674db86c2a2caaf9b5e9687098e660a8930f68
Seema-476 <simmybishnoi29085@gmail.com>
2024-03-19 10:10:46 +0530

    slider-2 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,24 @@ menuicon.addEventListener("click", function () {
     menulist.classList.toggle("show")
     document.body.classList.toggle("overflowhidden")
 })
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
 $('.responsive').slick({
     dots: true,
     infinite: true,
     speed: 300,
+    autoplay: true,
     slidesToShow: 2,
     slidesToScroll: 1,
     responsive: [
@@ -21,14 +35,21 @@ $('.responsive').slick({
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 815,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 1,
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 778,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 320,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
@@ -40,7 +61,7 @@ $('.responsive1').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    autoplay: false,
+    autoplay: true,
     slidesToShow: 2,
     slidesToScroll: 1,
     responsive: [
